e8631ccc8a7868ab94850b3768d301ab00a54aa3
Vukas82 <vukas82@gmail.com>
2019-10-25 10:47:36 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// Global app controller
+import string from './models/Search';
+
+import {
+    add,
+    multiply
+} from './views/searchView';
+
+
+console.log(string);
+
+console.log(multiply(3, 4));
+console.log(add(3, 4));
